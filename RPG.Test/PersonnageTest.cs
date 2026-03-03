f69e607c84c50f8b52b008223b7f15c1b619001f
Enzo Sandre <enzo@enzosandre.fr>
2026-03-03 16:28:41 +0100

    feat : Un personnage possède 10HP initiaux

---
+++ RPG.Test/PersonnageTest.cs
@@ -0,0 +1,25 @@
+﻿using RPG.UI;
+
+namespace RPG.Test;
+
+public class PersonnageTest
+{
+    /**
+     * Les personnages ont 10 points de vie
+        A zéro ils meurent.
+        Ils peuvent s’attaquer et perdent 1 HP à chaque coup.
+        
+     */
+
+    [Fact(DisplayName = "Les personnages ont 10HP initiaux")]
+    public void HpInitiaux()
+    {
+        // ETANT DONNE un personnage
+        var personnage = new Personnage();
+
+        // QUAND
+
+        // ALORS ses HP sont de 10
+        Assert.Equal(10, personnage.Hp);
+    }
+}
---
+++ RPG.UI/Personnage.cs
@@ -0,0 +1,6 @@
+﻿namespace RPG.UI;
+
+public class Personnage
+{
+    public int Hp { get; } = 10;
+}
